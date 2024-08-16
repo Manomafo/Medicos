7e86627b6085dd81c9ce830202d70dc72e5d9b01
Matthieu <166752810+Manomafo@users.noreply.github.com>
2024-08-16 19:14:09 -0300

    api medicos
    api medicos

---
+++ medicos/medicos/api/api/src/main/java/com/senacshoes/api/model/endereco/DadosEndereco.java
@@ -0,0 +1,6 @@
+package com.senacshoes.api.model.endereco;
+
+public record DadosEndereco(String logradouro, String bairro, String cep, String cidade, String uf, String numero,
+        String complemento) {
+
+}
